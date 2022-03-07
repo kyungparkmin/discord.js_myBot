2e49caeee4c66447fdba7b3c707c8bc2471f914c
박경민 <kyungparkmin@naver.com>
2022-03-07 16:57:15 +0900

    Update meal.js

--- commands/meal.js
+++ commands/meal.js
@@ -8,14 +8,9 @@ module.exports = {
     .setName("급식")
     .setDescription("급식을 알려줍니다"),
   async execute(interaction) {
-
-    
-    const result = await school.search(School.Region.GYEONGBUK, '봉양정보고등학교');
     school.init(School.Type.HIGH, School.Region.GYEONGBUK, 'R100000822');
 
     const meal = await school.getMeal();
-    console.log(`${meal.month}월 ${meal.day}일`);
-    console.log(meal);
 
     const Meal = new MessageEmbed()
       .setTitle(`${meal.month}월 ${meal.day}일`)
